79cc3304f7703cbdf86d5a8cbc22a97c3700db46
etienne <etienne.tillier@etu.umontpellier.fr>
2022-07-18 16:55:50 +0200

    heroku deploy

--- db.js
+++ db.js
@@ -1,23 +1,23 @@
 const Pool = require("pg").Pool;
 require("dotenv").config();
 
-const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  password: process.env.PASSWORD,
-  host: process.env.DATABASE_HOST,
-  port: 5432,
-  database: process.env.DATABASE_NAME,
-});
+// const pool = new Pool({
+//   user: process.env.DATABASE_USER,
+//   password: process.env.PASSWORD,
+//   host: process.env.DATABASE_HOST,
+//   port: 5432,
+//   database: process.env.DATABASE_NAME,
+// });
 
-// const herokuPool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// })
-
-
-module.exports = pool
+const herokuPool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 
-// module.exports = (process.env.PORT ? herokuPool : pool);
+// module.exports = pool
+
+
+module.exports = (process.env.PORT ? herokuPool : pool);
